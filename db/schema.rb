0eb130c470dabf73f9a87b265a5389aa1da9a592
Leo <leo.bravar95@gmail.com>
2021-07-09 00:21:02 +0200

    flight model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_212727) do
+ActiveRecord::Schema.define(version: 2021_07_08_221448) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.integer "start_id"
+    t.integer "finish_id"
+    t.datetime "departure"
+    t.integer "duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+airports = Airport.create([{code: "SFO"},{code: "NYC"}])
+
+Flight.create([
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-05 07:55", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-05 09:00", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-05 10:55", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-05 14:55", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-05 09:00", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-05 12:55", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-05 14:55", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-06 08:55", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-06 10:55", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-06 14:55", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-06 09:55", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-06 11:00", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-06 16:00", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-07 07:35", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-07 09:10", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-07 11:30", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "SFO").id, finish_id: Airport.find_by(code: "NYC").id, departure: "2022-07-07 15:05", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-07 07:35", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-07 09:30", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-07 12:15", duration: 60*60*7 },
+    { start_id: Airport.find_by(code: "NYC").id, finish_id: Airport.find_by(code: "SFO").id, departure: "2022-07-07 16:25", duration: 60*60*7 },
+  
+  ])
