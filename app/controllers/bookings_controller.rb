b3a1359d2f4d2f064cecdd75cf1d9ffd1164dac1
Leo <leo.bravar95@gmail.com>
2021-07-19 19:03:55 +0200

    booking model controller view and some other fixes

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+    def new
+        @booking = Booking.new
+        @flight = Flight.find(params[:flight_id])
+        params[:passengers].to_i.times { @booking.passengers.build }
+      end
+
+      def show
+        @booking = Booking.find(params[:id])
+      end
+    
+      def create
+        @booking = Booking.create(booking_params)
+        if @booking.save
+          redirect_to booking_path(@booking)
+        end
+      end
+    
+      private
+      def booking_params
+        params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
+    
+      end
+end
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,10 +5,23 @@ class FlightsController < ApplicationController
     end
 
     def index
+        @flight = Flight.new
         @flights = Flight.all
         @airports = Airport.all.map{ |a| [a.code, a.id] }
         @dates = Flight.all.map{ |f| [f.departure_date, f.departure_date] }.uniq
         @passengers = [[1,1],[2,2],[3,3],[4,4]]
+        if params[:flight]
+            @result = Flight.where(
+              start_id: params[:flight][:start_id],
+              finish_id: params[:flight][:finish_id],
+              departure_date: params[:flight][:departure_date]
+              ).to_a
+          end
+        end
 
+        private
+
+        def flight_params
+          params.require(:flight).permit(:start_id, :finish_id, :departure_date, :passengers)
+        end
     end
-end
